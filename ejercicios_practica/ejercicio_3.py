641c7bb25eb45a2cc3b0f764d10974652bfc7bf1
Santy-beep <santinorhv@hotmail.com>
2025-05-27 18:33:11 -0300

    Update ejercicio_3.py
    resolucion ejercicio 3

--- ejercicios_practica/ejercicio_3.py
+++ ejercicios_practica/ejercicio_3.py
@@ -15,25 +15,23 @@ def scatter_plot():
     # Demostración de la utilidad del scatter plot
     # Generaremos una linea y=x con ruido sumando
     # valores aleatorios uniformes en cada punto
-
-    sample_size = 20
-
-    x = np.arange(-np.pi, np.pi, 0.1)
-
+    
     #x = np.linspace(0, 10, sample_size)
     # A los valores de X sumar valores aleatoreos entre -1 y 1
     #y = x + np.random.uniform(-1, 1, sample_size)
-
+    x = np.arange(-np.pi, np.pi, 0.1)
     y = np.tanh(x)
-
 
     fig = plt.figure()
     fig.suptitle('Scatter', fontsize=16)
-    ax2 = fig.add_subplot(1, 2, 2)
 
-    ax2.scatter(x, y, c='darkcyan')
+    ax = fig.add_subplot()
+    
+    ax2.scatter(x, y, c='darkcyan', marker='o', label= 'y = tahn(x)')
     ax2.set_facecolor('whitesmoke')
     ax2.grid('solid')
+    ax.set_xlabel("X[rads]")
+    ax.set_ylabel("Y[amplitud]")
     plt.show()
     
 if __name__ == '__main__':
